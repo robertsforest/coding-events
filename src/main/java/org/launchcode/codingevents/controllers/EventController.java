fc1cd5c8fa59496a9675327eb348f2bc7a1755b1
robertsforest@gmail.com <C1r1isaderpderp>
2020-02-15 11:51:17 -0600

    Updated the Create method in the EventController to use Model Binding with a @ModelAttribute in the method signature. Added tags for field validation in the Event model class. Added email field.

--- src/main/java/org/launchcode/codingevents/controllers/EventController.java
+++ src/main/java/org/launchcode/codingevents/controllers/EventController.java
@@ -31,8 +31,8 @@ public class EventController {
     }
 
     @PostMapping("create")
-    public String processCreateEventForm(@RequestParam String eventName, @RequestParam String eventDescription) {
-        EventData.add(new Event(eventName, eventDescription));
+    public String processCreateEventForm(@ModelAttribute Event newEvent) {
+        EventData.add(newEvent);
         return "redirect:";
     }
 
--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,19 +1,29 @@
 package org.launchcode.codingevents.models;
 
+import javax.validation.constraints.Email;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.Objects;
 
 public class Event {
     private int id;
     private static int nextId = 1;
 
+    @Size(min = 2, max = 50, message = "Name must be between 2 and 50 characters.")
+    @NotBlank
     private String name;
+
+    @Size(max = 500, message = "Description too long!")
     private String description;
 
+    @Email(message = "Invalid email. Try again.")
+    private String contactEmail;
 
-    public Event(String name, String description) {
+    public Event(String name, String description, String contactEmail) {
         this.name = name;
         this.description = description;
         this.id = nextId;
+        this.contactEmail = contactEmail;
         nextId++;
     }
 
@@ -31,6 +41,14 @@ public class Event {
 
     public void setDescription(String description) {
         this.description = description;
+    }
+
+    public String getContactEmail() {
+        return contactEmail;
+    }
+
+    public void setContactEmail(String contactEmail) {
+        this.contactEmail = contactEmail;
     }
 
     public int getId() {
